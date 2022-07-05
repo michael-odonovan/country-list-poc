614b352e5ae857d5fa1c8ebacfd5b163ccae0cc1
michael o'donovan <mikeodonovan3@gmail.com>
2022-07-05 17:43:53 +0100

    add the CL

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { ThemeProvider, crTheme, Input } from '@comicrelief/component-library'
 
 export default function Index() {
   return (
-    <h2>hello world</h2>
+    <ThemeProvider theme={crTheme}>
+      <Input/>
+      <h2>hello world</h2>
+    </ThemeProvider>
   )
 }
