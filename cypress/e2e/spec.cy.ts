68856ab54be476c00b975be0fef89a19647714bf
michael o'donovan <mikeodonovan3@gmail.com>
2022-07-07 11:55:52 +0100

    feat: add npm country-lists package

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -3,6 +3,8 @@ describe('form with country list', () => {
   it('visits the base url', () => {
     cy.visit('/')
 
+    // test for Container component
+
     cy.get('h2').contains('Country (no western) List')
 
     cy.get('label').contains('name')
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,34 @@
 import React from 'react'
 import { ThemeProvider, crTheme, Input, Label, Select } from '@comicrelief/component-library'
 import Container from '../components/Container'
+import { ALL_COUNTRIES } from '@comicrelief/country-lists'
 
 export default function Index() {
+
+  console.log("UK list", ALL_COUNTRIES.getSelectItems())
+
   return (
     <ThemeProvider theme={crTheme}>
       <Container>
-        <h2>Country (no western) List</h2>
+
+        <h2>CR country list</h2>
+
         <Label label="name">
-          <Input/>
+          <Input name="name"/>
         </Label>
-        <Label>
-          <Input label="address"/>
+
+        <Label label="address">
+          <Input name="address"/>
         </Label>
+
         <Select
           label="country"
           errorMsg="This is an error message"
           description="Please choose your country"
           greyDescription
-          options={[
-            { value: 'Option one', displayValue: 'The first option' },
-            {
-              value: 'Option two',
-              displayValue: 'The second option'
-            },
-            { value: 'Option three', displayValue: 'The third option' },
-            { value: 'Option four', displayValue: 'The fourth option' }
-          ]}
+          options={ ALL_COUNTRIES.getSelectItems() }
         />
+
       </Container>
     </ThemeProvider>
   )
